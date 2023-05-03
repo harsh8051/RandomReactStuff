58fffb0347df4abbd95f00ffd36e64c4c7ebf75f
harsh8051 <105968770+harsh8051@users.noreply.github.com>
2023-05-03 22:59:43 +0530

    pdf setup
    Did basic setup for show pdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import MainWindow from "./DraggableComponentDemo/MainWindow";
+//import MainWindow from "./DraggableComponentDemo/MainWindow";
+import MainWindow from "./FileHandling/PdfFiles/MainWindow.jsx";
 
 export default function App() {
   return (
---
+++ src/FileHandling/PdfFiles/MainWindow.jsx
@@ -0,0 +1,34 @@
+import { React, useState } from "react";
+import { Page, Document } from 'react-pdf';
+
+export default function MainWindow() {
+  const [fileContent, setFileContent] = useState(null);
+  const [numPages, setNumPages] = useState(null);
+
+  function handleFileChange(event) {
+    const file = event.target.files[0];
+    console.log(file)
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onload = () => {
+      setFileContent(reader.result);
+    };
+    
+  }
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div>
+      <input type="file" accept=".pdf" onChange={handleFileChange} />
+      {fileContent && (
+        <Document file={fileContent} onLoadSuccess={onDocumentLoadSuccess}>
+          
+            <Page  pageNumber={1} />
+          
+        </Document>
+      )}
+    </div>
+  );
+}
