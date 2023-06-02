e50941ee733333e70fea5fbcaacd154931d050df
harsh8051 <105968770+harsh8051@users.noreply.github.com>
2023-06-02 22:52:58 +0530

    Make tab list using constant

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,44 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import DraggableWindow from "./DraggableComponentDemo/MainWindow";
 import TextWindow from "./TextDraft/MainWindow";
-import Header from './App/components/Header'
-import Container from './App/components/Container'
+import Header from "./App/components/Header";
+import Container from "./App/components/Container";
 import Main from "./Style.module.css";
 export default function App() {
+  const appList = [
+    {
+      title: "Editable",
+      link: "edit",
+      componentName: <TextWindow/>,
+    },
+    {
+      title: "Drag",
+      link: "drag",
+      componentName: <DraggableWindow/>,
+    },
+  ];
+
   return (
     <div className={Main.container}>
       <div className={Main.sidenav}>
         <Link to="/" className={Main.LinkTab}>
           Home
         </Link>
-        <Link to="/drag" className={Main.LinkTab}>
-          Drag
-        </Link>
-        <Link to="/edittable" className={Main.LinkTab}>
-          Editable
-        </Link>
+        {appList.map((app) => {
+          return (
+            <Link to={`/${app.link}`}className={Main.LinkTab}>
+              {app.title}
+            </Link>
+          );
+        })}
       </div>
       <div className={Main.main}>
         <Header />
         <Routes>
           <Route path="/" element={<Container />} />
-          <Route path="/drag" element={<DraggableWindow />} />
-          <Route path="/edittable" element={<TextWindow />} />
+          {appList.map((app)=>{
+            return(<Route path={`/${app.link}`} element={app.componentName} />)
+          })}
         </Routes>
       </div>
     </div>
