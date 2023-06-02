d336261405fbef6d2fde5205a6f2ac6ab7fb43ac
harsh8051 <105968770+harsh8051@users.noreply.github.com>
2023-06-02 21:29:35 +0530

    Rearrange component and  hight adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,29 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import DraggableWindow from "./DraggableComponentDemo/MainWindow";
 import TextWindow from "./TextDraft/MainWindow";
+import Header from './App/components/Header'
+import Container from './App/components/Container'
 import Main from "./Style.module.css";
 export default function App() {
   return (
     <div className={Main.container}>
-    <div className={Main.sidenav}>
-        <a href="drag">Drag</a>
-        <a href="#">Link</a>
-        <a href="#">Link</a>
+      <div className={Main.sidenav}>
+        <Link to="/" className={Main.LinkTab}>
+          Home
+        </Link>
+        <Link to="/drag" className={Main.LinkTab}>
+          Drag
+        </Link>
+        <Link to="/edittable" className={Main.LinkTab}>
+          Editable
+        </Link>
       </div>
       <div className={Main.main}>
-      <header className={Main.header}>
-        <h1>Test app real time update</h1>
-      </header>
-      
+        <Header />
         <Routes>
-          <Route path="/" element={<TextWindow />} />
+          <Route path="/" element={<Container />} />
           <Route path="/drag" element={<DraggableWindow />} />
+          <Route path="/edittable" element={<TextWindow />} />
         </Routes>
       </div>
     </div>
---
+++ src/App/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div><h1>Test app real time update</h1></div>
+  )
+}
